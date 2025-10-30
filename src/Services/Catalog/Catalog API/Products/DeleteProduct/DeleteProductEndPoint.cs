560d440e52efcd36fbf0f194451158ef2638da27
Fabio Pietrapertosa <fabio.pietrapertosa@bcsoft.net>
2025-10-30 16:11:51 +0100

    Develop API with CQRS: - Update Product - Delete Product

---
+++ src/Services/Catalog/Catalog API/Products/DeleteProduct/DeleteProductEndPoint.cs
@@ -0,0 +1,26 @@
+﻿
+using Catalog_API.Products.UpdateProduct;
+
+namespace Catalog_API.Products.DeleteProduct
+{
+    //public record DeleteProductRequest(Guid Id);
+    public record DeleteProductResponse(bool isSuccess);
+    public class DeleteProductEndPoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/products/{Id}", async (Guid Id, ISender sender) =>
+            {
+                var result = await sender.Send(new DeleteProductCommand(Id));
+                var response = result.Adapt<DeleteProductResponse>();
+                return Results.Ok(response);
+            })
+           .WithName("DeleteProduct")
+           .Produces<DeleteProductResponse>(StatusCodes.Status200OK)
+           .ProducesProblem(StatusCodes.Status400BadRequest)
+           .WithSummary("Delete Product")
+           .WithDescription("Delete Product");
+
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog API/Products/UpdateProduct/UpdateProductEndPoint.cs
@@ -0,0 +1,29 @@
+﻿namespace Catalog_API.Products.UpdateProduct;
+
+public record UpdateProductRequest(Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+public record UpdateProductResponse(bool isSuccess);
+
+public class UpdateProductEndPoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPut("/products", async (UpdateProductRequest request, ISender sender) =>
+        {
+            // Mappatura con Mapster
+            var command = request.Adapt<UpdateProductCommand>();
+            
+            //Dopo aver creato l'oggetto command, lo invii usando MediatR
+            var result = await sender.Send(command);
+            
+            //Dopo aver ricevuto il risultato dal comando, lo mappo alla risposta
+            var response = result.Adapt<UpdateProductResponse>();
+            return Results.Ok(response);
+        })
+        .WithName("UpdateProduct")
+        .Produces<UpdateProductResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Update Product")
+        .WithDescription("Update Product");
+    }
+}
+
